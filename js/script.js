c9a064111187766d68bedc8506b18829f46f943f
Boris Rozumniy <rozumniyboris@gmail.com>
2018-04-01 00:48:27 +0300

    changed the background image, the playing field moved to the center of the page, work continues on bugs with rapid clicks on the various elements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-let fromExternalScript = {"width":8,"height":4};
+let gameTable = document.querySelector('table');
+let fromExternalScript;
+let targets = [];
+let point = 0;
+let sumCells;
 
 const images = [
   "https://kde.link/test/0.png",
@@ -13,69 +17,68 @@ const images = [
   "https://kde.link/test/9.png"
 ];
 
-
-
-let lastClick = '';
-let previousTarget = [];
-let point = 0;
-let sumCells;
 gameField = () => {
-  if (document.querySelector('.gameTable')) {
-    document.querySelector('.gameTable').remove();
-  }
   let {width, height} = JSON.parse(fromExternalScript);
   sumCells = width * height;
+
   let copyImg = images.slice(0, sumCells / 2);
   copyImg.forEach(e => copyImg.push(e));
+  randomInteger = (min, max) => {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  };
   copyImg.sort( () => randomInteger(0, copyImg.length) );
-  let table = document.createElement('table');
-    for (let i = 0; width > i; i++) {
-      let row = table.insertRow(0);
-      for (let j = 0; height > j; j++) {
-        let cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.insertCell(j).appendChild(cell);
-      }
-    };
-    let cells = table.querySelectorAll('.cell');
-    for (let i = 0; cells.length > i; i++) {
-      let img = document.createElement('img');
-      img.classList.add('hidden');
-      img.src = copyImg[i];
-      cells[i].appendChild(img)
-    };
-    table.classList.add('gameTable');
-  document.body.appendChild(table);
+  let tBody = document.createElement('tbody');
+  tBody.classList.add('gameTable');
+  for (let i = 0; width > i; i++) {
+    let row = tBody.insertRow(0);
+    for (let j = 0; height > j; j++) {
+      let cell = row.insertCell(j);
+      cell.classList.add('cell');
+    }
+  };
+  let cells = tBody.querySelectorAll('.cell');
+  for (let i = 0; cells.length > i; i++) {
+    let img = document.createElement('img');
+    img.classList.add('hidden');
+    img.src = copyImg[i];
+    cells[i].appendChild(img);
+  }
+  gameTable.appendChild(tBody)
 
-
-  table.addEventListener('click', event => {
+  tBody.addEventListener('click', event => {
     let target = event.target;
     if (target.className != 'hidden') return;
-    previousTarget.push(target);
+    targets.push(target);
+    let [previous , curent, ...rest] = targets;
     target.classList.toggle('hidden');
 
-    // проверяем на совпадения
-    if (lastClick == target.src) {
-      setTimeout(() => {
-        target.parentElement.classList.toggle('hidden')
-        previousTarget[previousTarget.length-2].parentElement.classList.toggle('hidden');
-      }, 1200)
-      showPoint();
-      lastClick = '';
-      return;
-    }
-    if (lastClick == '') {
-      lastClick = target.src;
-      return;
-    }
+    if (targets.length > 1) {
 
-    if (lastClick !=target.src) {
-      lastClick = '';
-      setTimeout(() => {
-        target.classList.toggle('hidden')
-        previousTarget[previousTarget.length-2].classList.toggle('hidden');
-      }, 1200)
+      // проверяем на совпадения
+      if (previous.src == curent.src) {
+        let t1 = setTimeout(() => {
+          curent.parentElement.classList.toggle('hidden');
+          previous.parentElement.classList.toggle('hidden');
+          console.log('==', t1);
+        }, 1000)
+        showPoint();
+        return;
+      }
+
+      // проверяем на не совпадения
+      if (previous.src != curent.src) {
+        let t2 = setTimeout(() => {
+          curent.classList.toggle('hidden');
+          previous.classList.toggle('hidden');
+          console.log('!=', t2, target)
+          targets = [];
+        }, 1000)
+      }
+      rest.forEach(e => e.classList.toggle('hidden'));
     }
+    // console.log(rest, target.src)
   });
 };
 
@@ -84,17 +87,15 @@ showPoint = () => {
   point++;
   pointWiev.textContent = point;
   if (pointWiev.textContent == sumCells / 2) {
-    alert(`You win. Result: ${previousTarget.length}`)
+    alert(`You win. Result: ${targets.length}`)
+    clearField();
     gameField();
     point = 0;
-    previousTarget = [];
+    targets = [];
     pointWiev.textContent = point;
   }
 };
 
-
-randomInteger = (min, max) => {
-  var rand = min + Math.random() * (max + 1 - min);
-  rand = Math.floor(rand);
-  return rand;
+clearField = () => {
+  gameTable.children[0].remove()
 }
